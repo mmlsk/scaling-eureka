8889ca306fb01fd30b5c833d961382eca992da0d
mmlsk <domagalski.mat@gmail.com>
2026-04-16 21:18:27 +0200

    Remove output and basePath configurations

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,6 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  output: 'export',
-  basePath: '/scaling-eureka',
-  trailingSlash: true,
   reactStrictMode: true,
   images: {
     unoptimized: true,
